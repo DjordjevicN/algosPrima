c1fd6c0fe8c87b8f2d07770f88b1d84bc2de2823
Nikola <nikola.dj.87@gmail.com>
2025-02-19 18:12:38 +0100

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
-
+export default function bs_list(
+    haystack: number[],
+    trazeniBroj: number,
+): boolean {
+    let pocetak = 0;
+    let kraj = haystack.length;
+    do {
+        const sredina = Math.floor((pocetak + kraj) / 2);
+        const vrednost = haystack[sredina];
+        if (vrednost === trazeniBroj) {
+            return true;
+        } else if (vrednost > trazeniBroj) {
+            kraj = sredina;
+        } else {
+            pocetak = sredina + 1;
+        }
+    } while (pocetak < kraj);
+    return false;
 }
